f48a153ac65fce61c777e6c58be509724e0508ac
nolombic <67807622+nolombic@users.noreply.github.com>
2020-07-04 15:11:46 +0200

    Release 1.0
    With more_up_packs you're now able to have more health and breath points in minetest, as long as both the upgrade_packs mod by Krock and the mtg_plus mod by Wuzzy are both installed and active. The former is the base framework that allows this mod to function in the first place. The latter is used for the final +50HP upgrade.
    Upgrades range from +20HP to +50HP in steps of 10. There's currently also one upgrade that allows you to have +10 Breath so you can survive even longer unter water.

---
+++ init.lua
@@ -0,0 +1,95 @@
+local imgpath = minetest and minetest.get_modpath("more_up_packs") or "."
+
+upgrade_packs.register_pack("more_up_packs:hp_20", "health", {
+	description = "+20 HP",
+	strength = 20,
+	image = "green_heart.png"
+})
+
+local mc = "default:mese_crystal"
+local gi = "default:gold_ingot"
+local oh = "default:obsidian_shard"
+local h1 = "upgrade_packs:hp_10"
+minetest.register_craft({
+	output = "more_up_packs:hp_20",
+	recipe = {
+		{gi, mc, gi},
+		{mc, h1, mc},
+		{gi, oh, gi}
+	}
+})
+
+upgrade_packs.register_pack("more_up_packs:hp_30", "health", {
+	description = "+30 HP",
+	strength = 30,
+	image = "blue_heart.png"
+})
+
+local mc = "default:mese_crystal"
+local di = "default:diamond"
+local ob = "default:obsidian"
+local h2 = "more_up_packs:hp_20"
+minetest.register_craft({
+	output = "more_up_packs:hp_30",
+	recipe = {
+		{di, mc, di},
+		{ob, h2, ob},
+		{di, mc, di}
+	}
+})
+
+upgrade_packs.register_pack("more_up_packs:hp_40", "health", {
+	description = "+40 HP",
+	strength = 40,
+	image = "violet_heart.png"
+})
+
+local mb = "default:mese"
+local db = "default:diamondblock"
+local oc = "default:obsidianbrick"
+local h3 = "more_up_packs:hp_30"
+minetest.register_craft({
+	output = "more_up_packs:hp_40",
+	recipe = {
+		{db, mb, db},
+		{oc, h3, oc},
+		{db, mb, db}
+	}
+})
+
+upgrade_packs.register_pack("more_up_packs:hp_50", "health", {
+	description = "+50 HP",
+	strength = 50,
+	image = "golden_heart.png"
+})
+
+local mb = "default:mese"
+local gd = "mtg_plus:gold_diamond_block"
+local h4 = "more_up_packs:hp_40"
+local br = "mtg_plus:obsidianbrick_gold"
+minetest.register_craft({
+	output = "more_up_packs:hp_50",
+	recipe = {
+		{gd, mb, gd},
+		{br, h4, br},
+		{gd, mb, gd}
+	}
+})
+
+upgrade_packs.register_pack("more_up_packs:breath_10", "breath", {
+	description = "+10 Breath",
+	strength = 10,
+	image = "golden_bubble.png"
+})
+
+local mc = "default:mese_crystal"
+local b1 = "upgrade_packs:breath_5"
+local bi = "default:bronze_ingot"
+minetest.register_craft({
+	output = "more_up_packs:breath_10",
+	recipe = {
+		{bi, mc, bi},
+		{mc, b1, mc},
+		{bi, mc, bi}
+	}
+})
